f49e636840306448b13867d146429b9480e0f319
志翔 蘇 <suxen1094@gmail.com>
2020-11-19 23:04:55 +0800

    包含battery的初稿

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,7 +6,7 @@
 
 enum direction
 {
-	LEFT = 1, RIGHT, TOP, DOWN, NOTHING
+	LEFT = 1, RIGHT, TOP, DOWN, INITIAL
 };
 
 using namespace std;
@@ -130,104 +130,145 @@ public:
 	}
 
 	void DFS(MAP & mm, fstream & fout) {
-		stack<int> srow;
-		stack<int> scol;
+		while (mm.need_clean) {
+			stack<int> srow;
+			stack<int> scol;
+			stack<int> back_srow;
+			stack<int> back_scol;
+			stack<int> state;
+			int curStep = 0;
+			state.push(INITIAL);
+			srow.push(chargeRow);
+			scol.push(chargeCol);
+			while (!srow.empty()) {
+				if (!mm.need_clean) break;
+				int curRow = srow.top();
+				int curCol = scol.top();
+				int lastState = state.top();
+				bool go = false;
 
-		stack<int> back_srow;
-		stack<int> back_scol;
-		
-		stack<int> dir;
+				srow.pop();
+				scol.pop();
+				back_srow.push(curRow);
+				back_scol.push(curCol);
+				step++;
+				curStep++;
 
-		srow.push(chargeRow);
-		scol.push(chargeCol);
-		dir.push(NOTHING);
+				cout << curRow << ' ' << curCol << endl;
+				if (mm.map[curRow][curCol] == 0) {
+					mm.map[curRow][curCol] = 3;
+					mm.need_clean--;
+				}
+				mm.showmap();
+				if (curStep * 2 > battery) break;
 
-		while (!srow.empty()) {
-			if (!mm.need_clean) break;
-			int curRow = srow.top();
-			int curCol = scol.top();
-			int lastDir = dir.top();
-			bool go = false;
+				// find lefthand side(1)
+				if (curCol > 0) {
+					if (mm.map[curRow][curCol - 1] == 0) {
+						srow.push(curRow);
+						scol.push(curCol - 1);
+						state.push(LEFT);
+						go = true;
+					}
+				}
+				// find top side
+				if (curRow > 0 && !go) {
+					if (mm.map[curRow - 1][curCol] == 0) {
+						srow.push(curRow - 1);
+						scol.push(curCol);
+						state.push(TOP);
+						go = true;
+					}
+				}
+				// find righthand side
+				if (curCol < mm.col - 1 && !go) {
+					if (mm.map[curRow][curCol + 1] == 0) {
+						srow.push(curRow);
+						scol.push(curCol + 1);
+						state.push(RIGHT);
+						go = true;
+					}
+				}
+				// find down side
+				if (curRow < mm.row - 1 && !go) {
+					if (mm.map[curRow + 1][curCol] == 0) {
+						srow.push(curRow + 1);
+						scol.push(curCol);
+						state.push(DOWN);
+						go = true;
+					}
+				}
 
-			srow.pop();
-			scol.pop();
-			back_srow.push(curRow);
-			back_scol.push(curCol);
-			step++;
-			cout << curRow << ' ' << curCol << endl;
-			if (mm.map[curRow][curCol] == 0) {
-				mm.map[curRow][curCol] = 3;
-				mm.need_clean--;
+				if (!go) {
+					if (lastState == LEFT) {
+						srow.push(curRow);
+						scol.push(curCol + 1);
+					}
+					else if (lastState == TOP) {
+						srow.push(curRow + 1);
+						scol.push(curCol);
+					}
+					else if (lastState == RIGHT) {
+						srow.push(curRow);
+						scol.push(curCol - 1);
+					}
+					else if (lastState == DOWN) {
+						srow.push(curRow - 1);
+						scol.push(curCol);
+					}
+					else if (lastState == INITIAL) {
+						bool ini_go = false;
+						// find down side
+						if (curRow < mm.row - 1) {
+							if (mm.map[curRow + 1][curCol] == 3) {
+								srow.push(curRow + 1);
+								scol.push(curCol);
+								state.push(INITIAL);
+								ini_go = true;
+							}
+						}
+						// find righthand side
+						if (curCol < mm.col - 1 && !ini_go) {
+							if (mm.map[curRow][curCol + 1] == 3) {
+								srow.push(curRow);
+								scol.push(curCol + 1);
+								state.push(INITIAL);
+								ini_go = true;
+							}
+						}
+						// find top side
+						if (curRow > 0 && !ini_go) {
+							if (mm.map[curRow - 1][curCol] == 3) {
+								srow.push(curRow - 1);
+								scol.push(curCol);
+								state.push(INITIAL);
+								ini_go = true;
+							}
+						}
+						// find lefthand side(1)
+						if (curCol > 0 && !ini_go) {
+							if (mm.map[curRow][curCol - 1] == 3) {
+								srow.push(curRow);
+								scol.push(curCol - 1);
+								state.push(INITIAL);
+								go = true;
+							}
+						}
+					}
+				}
 			}
-			
-			// find lefthand side(1)
-			if (curCol > 0) {
-				if (mm.map[curRow][curCol - 1] == 0) {
-					srow.push(curRow);
-					scol.push(curCol - 1);
-					dir.push(LEFT);	
-					go = true;
-				}
-			}			
-			// find top side
-			if (curRow > 0 && !go) {
-				if (mm.map[curRow - 1][curCol] == 0) {
-					srow.push(curRow - 1);
-					scol.push(curCol);
-					dir.push(TOP);
-					go = true;
-				}
-			}
-			// find righthand side
-			if (curCol < mm.col - 1 && !go) {
-				if (mm.map[curRow][curCol + 1] == 0) {
-					srow.push(curRow);
-					scol.push(curCol + 1);
-					dir.push(RIGHT);
-					go = true;
-				}
-			}
-			// find down side
-			if (curRow < mm.row - 1 && !go) {
-				if (mm.map[curRow + 1][curCol] == 0) {
-					srow.push(curRow + 1);
-					scol.push(curCol);
-					dir.push(DOWN);
-					go = true;
-				}
-			}
 
-			if (!go) {
-				if (lastDir == LEFT) {
-					srow.push(curRow);
-					scol.push(curCol + 1);
-				}
-				else if (lastDir = TOP) {
-					srow.push(curRow + 1);
-					scol.push(curCol);
-				}
-				else if (lastDir = RIGHT) {
-					srow.push(curRow);
-					scol.push(curCol - 1);
-				}
-				else if (lastDir = DOWN) {
-					srow.push(curRow - 1);
-					scol.push(curCol);
-				}
-			}
-		}
-
-		// go back to charge point
-		// pop掉重複的點
-		back_srow.pop();
-		back_scol.pop();
-		while (!back_srow.empty()) {
-			step++;
-			cout << back_srow.top() << ' ' << back_scol.top() << endl;
+			// go back to charge point
+			// pop掉重複的點
 			back_srow.pop();
 			back_scol.pop();
+			while (!back_srow.empty()) {
+				step++;
+				cout << back_srow.top() << ' ' << back_scol.top() << endl;
+				back_srow.pop();
+				back_scol.pop();
+			}
 		}
-		fout << step;
 	}
 
 	void record(fstream & fout) {
@@ -252,7 +293,6 @@ int main() {
 	MAP mm(m, n);
 	robot r(B);
 	mm.read(fin);
-	while(mm.need_clean)
-		r.DFS(mm, fout);
+	r.DFS(mm, fout);
 	mm.showmap();
 }
