620fa348768e793d2a8067049cc9a44a998c80ab
志翔 蘇 <suxen1094@gmail.com>
2020-11-26 23:47:29 +0800

    就這樣吧

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,17 +3,13 @@
 #include <string>
 #include <queue>
 #include <stack>
-
-enum direction
-{
-	LEFT = 1, RIGHT, TOP, DOWN, INITIAL
-};
+#include <vector>
 
 using namespace std;
-int chargeRow, chargeCol;
 queue<int> qrow;
 queue<int> qcol;
 queue<int> qdist;
+vector<int> v;
 
 class MAP {
 public:
@@ -21,10 +17,16 @@ public:
 	MAP(int m, int n): row(m), col(n) {
 		map = new int*[m];
 		dist = new int*[m];
+		visited = new int*[m];
+		dfs_visited = new int*[m];
 		for (int i = 0; i < m; i++) {
 			map[i] = new int[n];
 			dist[i] = new int[n];
+			visited[i] = new int[n];
+			dfs_visited[i] = new int[n];
 			fill(dist[i], dist[i] + n, 0);
+			fill(visited[i], visited[i] + n, 1);
+			fill(dfs_visited[i], dfs_visited[i] + n, 1);
 		}
 	}
 
@@ -42,6 +44,8 @@ public:
 					(this->map)[i][j] = c - '0';
 					if (c == '0') {
 						need_clean++;
+						visited[i][j] = 0;
+						dfs_visited[i][j] = 0;
 					}
 				}
 			}
@@ -49,11 +53,11 @@ public:
 	}
 
 	// used to debug
-	void showmap() {
-		cout << "map:\n\n";
+	void showmap(int ** m) {
+		cout << "\n\n";
 		for (int i = 0; i < this->row; i++) {
 			for (int j = 0; j < this->col; j++) {
-				cout << this->map[i][j];
+				cout << m[i][j];
 			}
 			cout << endl;
 		}
@@ -71,11 +75,21 @@ public:
 		cout << "\n----------------------\n\n";
 	}
 
+	void showdistline(int num) {
+		for (int i = 0; i < col; i++)
+			cout << dist[num][i];
+		cout << "\n----------------------\n\n";
+	}
+
+
 	int row;
 	int col;
 	int **map;
 	int need_clean = 0;
 	int **dist;
+	int **visited;
+	int **dfs_visited;
+	int chargeRow, chargeCol;
 };
 
 class robot {
@@ -83,22 +97,21 @@ public:
 	robot(int B) : battery(B) {}
 
 	void BFS(MAP & mm) {
-		qrow.push(chargeRow);
-		qcol.push(chargeCol);
-
+		qrow.push(mm.chargeRow);
+		qcol.push(mm.chargeCol);
 		while (!qrow.empty()) {
 			int curRow = qrow.front();
 			int curCol = qcol.front();
 			int curDist = mm.dist[curRow][curCol];
 			qrow.pop();
 			qcol.pop();
-			mm.map[curRow][curCol] = 3;
-
+			//mm.showmap();
 			// find lefthand side
 			if (curCol > 0) {
 				if (mm.map[curRow][curCol - 1] == 0) {
 					qrow.push(curRow);
 					qcol.push(curCol - 1);
+					mm.map[curRow][curCol - 1] = 3;
 					mm.dist[curRow][curCol - 1] = curDist + 1;
 				}
 			}
@@ -107,6 +120,7 @@ public:
 				if (mm.map[curRow - 1][curCol] == 0) {
 					qrow.push(curRow - 1);
 					qcol.push(curCol);
+					mm.map[curRow - 1][curCol] = 3;
 					mm.dist[curRow - 1][curCol] = curDist + 1;
 				}
 			}
@@ -115,6 +129,7 @@ public:
 				if (mm.map[curRow][curCol + 1] == 0) {
 					qrow.push(curRow);
 					qcol.push(curCol + 1);
+					mm.map[curRow][curCol + 1] = 3;
 					mm.dist[curRow][curCol + 1] = curDist + 1;
 				}
 			}
@@ -123,6 +138,7 @@ public:
 				if (mm.map[curRow + 1][curCol] == 0) {
 					qrow.push(curRow + 1);
 					qcol.push(curCol);
+					mm.map[curRow + 1][curCol] = 3;
 					mm.dist[curRow + 1][curCol] = curDist + 1;
 				}
 			}
@@ -130,161 +146,116 @@ public:
 	}
 
 	void DFS(MAP & mm, fstream & fout) {
+
 		while (mm.need_clean) {
-			stack<int> srow;
-			stack<int> scol;
-			stack<int> back_srow;
-			stack<int> back_scol;
-			stack<int> state;
-			int curStep = 0;
-			state.push(INITIAL);
-			srow.push(chargeRow);
-			scol.push(chargeCol);
-			while (!srow.empty()) {
-				if (!mm.need_clean) break;
-				int curRow = srow.top();
-				int curCol = scol.top();
-				int lastState = state.top();
-				bool go = false;
+			find = false;
+			int MAX = 0;
+			int ** k = new int *[mm.row];
 
-				srow.pop();
-				scol.pop();
-				back_srow.push(curRow);
-				back_scol.push(curCol);
-				step++;
-				curStep++;
-
-				cout << curRow << ' ' << curCol << endl;
-				if (mm.map[curRow][curCol] == 0) {
-					mm.map[curRow][curCol] = 3;
-					mm.need_clean--;
-				}
-				mm.showmap();
-				if (curStep * 2 > battery) break;
-
-				// find lefthand side(1)
-				if (curCol > 0) {
-					if (mm.map[curRow][curCol - 1] == 0) {
-						srow.push(curRow);
-						scol.push(curCol - 1);
-						state.push(LEFT);
-						go = true;
-					}
-				}
-				// find top side
-				if (curRow > 0 && !go) {
-					if (mm.map[curRow - 1][curCol] == 0) {
-						srow.push(curRow - 1);
-						scol.push(curCol);
-						state.push(TOP);
-						go = true;
-					}
-				}
-				// find righthand side
-				if (curCol < mm.col - 1 && !go) {
-					if (mm.map[curRow][curCol + 1] == 0) {
-						srow.push(curRow);
-						scol.push(curCol + 1);
-						state.push(RIGHT);
-						go = true;
-					}
-				}
-				// find down side
-				if (curRow < mm.row - 1 && !go) {
-					if (mm.map[curRow + 1][curCol] == 0) {
-						srow.push(curRow + 1);
-						scol.push(curCol);
-						state.push(DOWN);
-						go = true;
-					}
-				}
-
-				if (!go) {
-					if (lastState == LEFT) {
-						srow.push(curRow);
-						scol.push(curCol + 1);
-					}
-					else if (lastState == TOP) {
-						srow.push(curRow + 1);
-						scol.push(curCol);
-					}
-					else if (lastState == RIGHT) {
-						srow.push(curRow);
-						scol.push(curCol - 1);
-					}
-					else if (lastState == DOWN) {
-						srow.push(curRow - 1);
-						scol.push(curCol);
-					}
-					else if (lastState == INITIAL) {
-						bool ini_go = false;
-						// find down side
-						if (curRow < mm.row - 1) {
-							if (mm.map[curRow + 1][curCol] == 3) {
-								srow.push(curRow + 1);
-								scol.push(curCol);
-								state.push(INITIAL);
-								ini_go = true;
-							}
-						}
-						// find righthand side
-						if (curCol < mm.col - 1 && !ini_go) {
-							if (mm.map[curRow][curCol + 1] == 3) {
-								srow.push(curRow);
-								scol.push(curCol + 1);
-								state.push(INITIAL);
-								ini_go = true;
-							}
-						}
-						// find top side
-						if (curRow > 0 && !ini_go) {
-							if (mm.map[curRow - 1][curCol] == 3) {
-								srow.push(curRow - 1);
-								scol.push(curCol);
-								state.push(INITIAL);
-								ini_go = true;
-							}
-						}
-						// find lefthand side(1)
-						if (curCol > 0 && !ini_go) {
-							if (mm.map[curRow][curCol - 1] == 3) {
-								srow.push(curRow);
-								scol.push(curCol - 1);
-								state.push(INITIAL);
-								go = true;
-							}
+			// find the point with biggest distance
+			for (int i = 0; i < mm.row; i++) {
+				k[i] = new int[mm.col];
+				for (int j = 0; j < mm.col; j++) {
+					k[i][j] = mm.dfs_visited[i][j];
+					if (mm.visited[i][j] == 0) {
+						if (MAX < mm.dist[i][j]) {
+							far_row = i;
+							far_col = j;
+							MAX = mm.dist[i][j];
 						}
 					}
 				}
 			}
+			
+			// find the path from charge point to far point
+			findpath_dfs(mm, mm.chargeRow, mm.chargeCol, k, 0);
 
-			// go back to charge point
-			// pop±¼­«½ÆªºÂI
-			back_srow.pop();
-			back_scol.pop();
-			while (!back_srow.empty()) {
-				step++;
-				cout << back_srow.top() << ' ' << back_scol.top() << endl;
-				back_srow.pop();
-				back_scol.pop();
+			record(mm);
+		}
+
+		orderc.push(mm.chargeCol);
+		orderr.push(mm.chargeRow);
+	}
+
+	void findpath_dfs(MAP &mm, int curRow, int curCol, int ** dfs_visit, int curstep) {
+		sc.push(curCol);
+		sr.push(curRow);
+		if (curstep > mm.dist[far_row][far_col]) return;
+		if (curRow == far_row && curCol == far_col) {
+			find = true;
+			return;
+		}
+
+		// left
+		if (curCol > 0 && find == false) {
+			if (dfs_visit[curRow][curCol - 1] == 0) {
+				dfs_visit[curRow][curCol - 1] = 1;
+				findpath_dfs(mm, curRow, curCol - 1, dfs_visit, curstep + 1);
 			}
+		}
+
+		// right
+		if (curCol < mm.col - 1 && find == false) {
+			if (dfs_visit[curRow][curCol + 1] == 0) {
+				dfs_visit[curRow][curCol + 1] = 1;
+				findpath_dfs(mm, curRow, curCol + 1, dfs_visit, curstep + 1);
+			}
+		}
+
+		// top
+		if (curRow > 0 && find == false) {
+			if (dfs_visit[curRow - 1][curCol] == 0) {
+				dfs_visit[curRow - 1][curCol] = 1;
+				findpath_dfs(mm, curRow - 1, curCol, dfs_visit, curstep + 1);
+			}
+		}
+
+		// down
+		if (curRow < mm.row - 1 && find == false) {
+			if (dfs_visit[curRow + 1][curCol] == 0) {
+				dfs_visit[curRow + 1][curCol] = 1; 
+				findpath_dfs(mm, curRow + 1, curCol, dfs_visit, curstep + 1);
+			}
+		}
+		if (find) {
+			sr.push(curRow);
+			sc.push(curCol);
+		}
+		else {
+			sr.pop();
+			sc.pop();
 		}
 	}
 
-	void record(fstream & fout) {
-		fout << step << endl;
+	void record(MAP &mm) {
+		while (!sr.empty()) {
+			step++;
+			int r = sr.top();
+			int c = sc.top();
+			sr.pop();
+			sc.pop();
+			orderr.push(r);
+			orderc.push(c);
+			if (mm.visited[r][c] == 0) {
+				mm.need_clean--;
+			}
+			mm.visited[r][c] = 1;
+		}
 	}
 
+	bool find = false;
 	int battery;
-	int step = 0;
-	int robotRow, robotCol;
+	int step = 0; 
+	stack<int> sr, sc;
+	queue<int> orderr, orderc;
+	int far_row = 0, far_col = 0;
 };
 
 int main() {
 	// file operation
 	fstream fin, fout;
-	fin.open("108062226.data", ios::in);
-	fout.open("final.path", ios::out);
+	fin.open("floor.data", ios::in);
+	fout.open("3.path", ios::out);
 	int m, n, B;
 	fin >> m >> n >> B;
 	cout << "m = " << m << ", n = " << n << ", B = " << B << endl; // debug
@@ -293,6 +264,15 @@ int main() {
 	MAP mm(m, n);
 	robot r(B);
 	mm.read(fin);
+	mm.showmap(mm.dfs_visited);
+	mm.showmap(mm.visited);
+	r.BFS(mm);
 	r.DFS(mm, fout);
-	mm.showmap();
+
+	fout << r.step << endl;
+	while (!r.orderr.empty()) {
+		fout << r.orderr.front() << " " << r.orderc.front() << endl;
+		r.orderc.pop();
+		r.orderr.pop();
+	}
 }
